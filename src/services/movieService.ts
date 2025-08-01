c7f3e53e13a89d6469917a60dfba552a3e25046a
Dmode <dmode90@gmail.com>
2025-08-01 14:42:30 +0300

    initial setup

---
+++ src/services/movieService.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import type { Movie } from "../types/types";
+
+interface ApiSearchResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export const API_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3/search/";
+axios.defaults.headers.common["Authorization"] = "Bearer " + API_TOKEN;
+
+export async function fetchMovies(query: string): Promise<ApiSearchResponse> {
+  const response = await axios.get(`movie?query=${query}`);
+  return response.data;
+}
